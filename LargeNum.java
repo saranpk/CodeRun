5e448c18de0cf2b4414f9a67aa68129e8f716e7e
saranpk <rsaranya101996@gmail.com>
2017-06-20 16:02:27 +0530

    Create LargeNum.java

---
+++ LargeNum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class LargeNum {
+	public static void main(String[] args){
+		int a,b,c;
+		System.out.println("Enter 3 numbers");
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		c=s.nextInt();
+		if(a>b&&a>c){
+			System.out.println(a+ "is a greater number");
+		}
+		else if(b>c){
+			System.out.println(b+ "is a greater number");
+		}
+		else{
+			System.out.println(c+ "is a greater number");
+		}
+	}
+
+}
