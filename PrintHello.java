ba3b8096808db60d212ccb07cd39d8d1f0b2ece2
saranpk <rsaranya101996@gmail.com>
2017-06-20 16:34:14 +0530

    Create PrintHello.java

---
+++ PrintHello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class PrintHello {
+	public static void main(String[] args){
+		int i;
+		for(i=0;i<5;i++){
+			System.out.println("Hello");
+		}
+	}
+}
