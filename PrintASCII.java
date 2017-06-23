26a0e5f20617dd25830363279ad89ecc72877b3f
saranpk <rsaranya101996@gmail.com>
2017-06-23 09:45:09 +0530

    Create PrintASCII.java

---
+++ PrintASCII.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class PrintASCII {
+	public static void main(String[] args){
+		int i;
+		//int j;
+		for(i=65;i<=91;i++){
+			System.out.println();
+		}
+	}
+
+}
